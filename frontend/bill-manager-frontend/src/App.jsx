e8a52d2d671ba1fd0d219010cf5aa9754144f440
Dhvai365 <dhvai365@gmail.com>
2025-01-26 22:15:31 +0530

    commit: Created a proper UI structure

--- frontend/bill-manager-frontend/src/App.jsx
+++ frontend/bill-manager-frontend/src/App.jsx
@@ -1,7 +1,5 @@
-// Filename: App.jsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import  Homepage  from './pages/Homepage'
-import './App.css'
 import Layout from './Layout'
 
 const App = () => {
