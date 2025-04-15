357d0cb32ce24d4d62daa9a715d960c7d2a1558a
Dhvani Maktuporia <91522146+Dhvani365@users.noreply.github.com>
2025-04-15 21:37:04 +0530

    fix: removed auth middleware logic

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,22 +3,22 @@ import User from "../models/user.model.js";
  
 export const protectRoute = async(req, res, next) => {
     try {
-        let token = null
-        try {
-            token = req.cookies.jwt;
-        } catch (error) {
-            token = null
-            return res.status(401).json({message: "Unauthorized - No Token Provided"})
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        if (!decoded)
-        {
-            return res.status(401).json({message: "Unauthorized - Invalid Token"})
-        }
+        // let token = null
+        // try {
+        //     token = req.cookies.jwt;
+        // } catch (error) {
+        //     token = null
+        //     return res.status(401).json({message: "Unauthorized - No Token Provided"})
+        // }
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        // if (!decoded)
+        // {
+        //     return res.status(401).json({message: "Unauthorized - Invalid Token"})
+        // }
 
-        const user = await User.findById(decoded.userId);
+        // const user = await User.findById(decoded.userId);
 
-        req.user = user;
+        // req.user = user;
 
         next()
     } catch (error) {
