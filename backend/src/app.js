8af0907efd5bf2ad673516105a7e3fccd4de1da4
Dhvani Maktuporia <91522146+Dhvani365@users.noreply.github.com>
2025-04-16 21:29:23 +0530

    fix: CORS Error fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,15 +15,31 @@ import billRoutes from "./routes/bill.route.js"
 import {protectRoute} from './middlewares/auth.middleware.js';
 import { connectDb } from "./lib/db.js";
 
+const allowedOrigins = [
+    "https://tmc-bill-website.onrender.com",
+    "http://localhost:5173" // for development
+  ];
+
 dotenv.config({ path: '../.env' });
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const app = express();
 
 app.use(cors({
-    origin: FRONTEND_URL,
+    origin: allowedOrigins,
     credentials: true
 }));
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
+    
 
 app.use(express.json());
 app.use(cookieParser())
